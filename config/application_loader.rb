a615c5b8f0a18b6705b22808ac5af0831b95bfd9
Vitaliy Tokmyanin <shamik2024@gmail.com>
2021-04-03 18:31:13 +0300

    fix_loader

--- config/application_loader.rb
+++ config/application_loader.rb
@@ -3,7 +3,6 @@ module ApplicationLoader
 
   def load_app!
     init_config
-    init_db
     require_app
     init_app
   end
@@ -14,14 +13,9 @@ module ApplicationLoader
     require_file 'config/initializers/config'
   end
 
-  def init_db
-    require_file 'config/initializers/db'
-  end
-
   def require_app
     require_dir 'app/helpers'
     require_file 'config/application'
-    require_file 'app/services/basic_service'
     require_dir 'app/contracts'
     require_dir 'app'
   end
--- config/initializers/active_support.rb
+++ config/initializers/active_support.rb
@@ -1,2 +1,1 @@
 require 'active_support/core_ext/object/blank'
-require 'active_support/core_ext/object/to_query'
