1cded1f12b96b14fb04963c4e9a9ddb39abc9b6d
Vitaliy Tokmyanin <shamik2024@gmail.com>
2021-04-03 18:47:41 +0300

    rm_pp

--- app/routes/geocoder_routes.rb
+++ app/routes/geocoder_routes.rb
@@ -2,7 +2,6 @@ class GeocoderRoutes < Application
   namespace '/v1' do
     post do
       result = Geocoder.geocode(params['city'])
-      pp result
       response.status = 200
       result.to_json
     end
