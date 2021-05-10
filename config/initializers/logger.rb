c29a282ab550554c6c34b94570b35da0c72291dc
Vitaliy Tokmyanin <shamik2024@gmail.com>
2021-05-10 22:19:52 +0300

    dockerize_and_set_deploy

---
+++ config/initializers/logger.rb
@@ -0,0 +1,15 @@
+logdev = Application.environment.eql?('production') ? STDOUT : Application.root.concat('/', Settings.logger.path)
+
+logger = Ougai::Logger.new(
+  logdev,
+  level: Settings.logger.level
+)
+
+logger.formatter = Ougai::Formatters::Readable.new
+
+logger.before_log = lambda do |data|
+  data[:service] = { name: Settings.app.name }
+  data[:request_id] ||= Thread.current[:request_id]
+end
+
+Application.logger = logger
